e25311a1f3129c573df6e6ccdae400826813b68c
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-06-28 14:21:54 -0700

    adding links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,20 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { gql, useQuery } from '@apollo/client';
+
 import { initializeApollo } from '../lib/client';
+import { TasksDocument, TasksQuery, useTasksQuery } from '../generated/graphql-fontend';
 
-const TasksQueryDocument = gql`
-  query Tasks {
-    tasks {
-      id
-      title
-      status
-    }
-  }
-`;
-
-interface TasksQuery {
-  tasks: { id: number; title: string; status: string }[];
-}
 
 export default function Home() {
-  const result = useQuery<TasksQuery>(TasksQueryDocument);
+  const result = useTasksQuery();
 console.log(result.data)
   const tasks = result.data?.tasks;
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Tasks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {console.log(tasks)}
       {tasks &&
         tasks.length > 0 &&
         tasks.map((task) => {
@@ -46,7 +32,7 @@ export const getStaticProps = async () => {
   const apolloClient = initializeApollo();
 
   await apolloClient.query<TasksQuery>({
-    query: TasksQueryDocument,
+    query: TasksDocument,
   });
 
   return {
